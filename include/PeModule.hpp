1124e97e7e69865afb735f2cf3bb55926c01318c
MichalMroz <125133223+MichalMroz21@users.noreply.github.com>
2024-03-05 23:36:42 +0100

    PeModule added

--- include/PeModule.hpp
+++ include/PeModule.hpp
@@ -9,29 +9,9 @@
 class PeModule {
 public:
 
-	PeModule(std::string name, std::vector<uint8_t> data) {
-		this->name = name;
-		this->data = data;
+	PeModule(std::string name, std::vector<uint8_t> data);
 
-		convertBytesToPE();
-
-		PE = PeLib::openPeFile("temp_pe.exe");
-	}
-
-	void convertBytesToPE() {
-		std::ofstream outFile("temp_pe.exe", std::ios::binary | std::ios::trunc);
-
-		if (outFile.is_open()) {
-			outFile.write(reinterpret_cast<char*>(this->data.data()), this->data.size());
-			outFile.close();
-
-			std::cout << "PE file saved to temp_pe.exe" << std::endl;
-		}
-		else {
-			std::cerr << "Error: Unable to open temp_pe.exe for writing" << std::endl;
-			return;
-		}
-	}
+	void convertBytesToPE();
 
 	std::string name{};
 	std::vector<uint8_t> data{};
---
+++ source/PeModule.cpp
@@ -0,0 +1,26 @@
+#include "PeModule.hpp"
+
+
+PeModule::PeModule(std::string name, std::vector<uint8_t> data) {
+	this->name = name;
+	this->data = data;
+
+	convertBytesToPE();
+
+	PE = PeLib::openPeFile("temp_pe.exe");
+}
+
+void PeModule::convertBytesToPE() {
+	std::ofstream outFile("temp_pe.exe", std::ios::binary | std::ios::trunc);
+
+	if (outFile.is_open()) {
+		outFile.write(reinterpret_cast<char*>(this->data.data()), this->data.size());
+		outFile.close();
+
+		std::cout << "PE file saved to temp_pe.exe" << std::endl;
+	}
+	else {
+		std::cerr << "Error: Unable to open temp_pe.exe for writing" << std::endl;
+		return;
+	}
+}
